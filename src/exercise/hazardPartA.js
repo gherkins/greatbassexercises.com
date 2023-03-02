d72b97a89ca2762821cd9b22501d57671e610b44
Max Girkens <maxgirkens@gmail.com>
2023-03-02 10:45:39 +0100

    hazard exercise part A

---
+++ src/exercise/hazardPartA.js
@@ -0,0 +1,298 @@
+const hazardPartA = {
+  title: `Steve Bailey´s Hazard Exercise Part A`,
+  description: `"It's like yoga - for your fingers..."\nPractice at your own risk.`,
+  bars: [
+    {
+      chord: [],
+      ticks: [
+        [{
+          string: 1,
+          fret: 1,
+          finger: 1,
+        }],
+        [{
+          string: 1,
+          fret: 2,
+          finger: 2,
+        }],
+
+        [{
+          string: 2,
+          fret: 1,
+          finger: 1,
+        }],
+        [{
+          string: 2,
+          fret: 2,
+          finger: 2,
+        }],
+
+        [{
+          string: 3,
+          fret: 1,
+          finger: 1,
+        }],
+        [{
+          string: 3,
+          fret: 2,
+          finger: 2,
+        }],
+
+        [{
+          string: 4,
+          fret: 1,
+          finger: 1,
+        }],
+        [{
+          string: 4,
+          fret: 2,
+          finger: 2,
+        }],
+
+        [
+          {
+            string: 4,
+            fret: 1,
+            finger: 1,
+          },
+          {
+            string: 4,
+            fret: 3,
+            finger: 3,
+          },
+        ],
+        [
+          {
+            string: 4,
+            fret: 1,
+            finger: 1,
+          },
+          {
+            string: 4,
+            fret: 2,
+            finger: 2,
+          },
+        ],
+
+        [
+          {
+            string: 4,
+            fret: 1,
+            finger: 1,
+          },
+          {
+            string: 3,
+            fret: 3,
+            finger: 3,
+          },
+        ],
+        [
+          {
+            string: 4,
+            fret: 1,
+            finger: 1,
+          },
+          {
+            string: 3,
+            fret: 2,
+            finger: 2,
+          },
+        ],
+
+        [
+          {
+            string: 4,
+            fret: 1,
+            finger: 1,
+          },
+          {
+            string: 2,
+            fret: 3,
+            finger: 3,
+          },
+        ],
+        [
+          {
+            string: 4,
+            fret: 1,
+            finger: 1,
+          },
+          {
+            string: 2,
+            fret: 2,
+            finger: 2,
+          },
+        ],
+
+        [
+          {
+            string: 4,
+            fret: 1,
+            finger: 1,
+          },
+          {
+            string: 1,
+            fret: 3,
+            finger: 3,
+          },
+        ],
+        [
+          {
+            string: 4,
+            fret: 1,
+            finger: 1,
+          },
+          {
+            string: 1,
+            fret: 2,
+            finger: 2,
+          },
+        ],
+
+
+        [
+          {
+            string: 4,
+            fret: 1,
+            finger: 1,
+          },
+          {
+            string: 1,
+            fret: 2,
+            finger: 2,
+          },
+          {
+            string: 1,
+            fret: 3,
+            finger: 3,
+          },
+        ],
+        [
+          {
+            string: 4,
+            fret: 1,
+            finger: 1,
+          },
+          {
+            string: 1,
+            fret: 2,
+            finger: 2,
+          },
+          {
+            string: 1,
+            fret: 4,
+            finger: 4,
+          },
+        ],
+
+        [
+          {
+            string: 4,
+            fret: 1,
+            finger: 1,
+          },
+          {
+            string: 1,
+            fret: 2,
+            finger: 2,
+          },
+          {
+            string: 2,
+            fret: 3,
+            finger: 3,
+          },
+        ],
+        [
+          {
+            string: 4,
+            fret: 1,
+            finger: 1,
+          },
+          {
+            string: 1,
+            fret: 2,
+            finger: 2,
+          },
+          {
+            string: 2,
+            fret: 4,
+            finger: 4,
+          },
+        ],
+
+        [
+          {
+            string: 4,
+            fret: 1,
+            finger: 1,
+          },
+          {
+            string: 1,
+            fret: 2,
+            finger: 2,
+          },
+          {
+            string: 3,
+            fret: 3,
+            finger: 3,
+          },
+        ],
+        [
+          {
+            string: 4,
+            fret: 1,
+            finger: 1,
+          },
+          {
+            string: 1,
+            fret: 2,
+            finger: 2,
+          },
+          {
+            string: 3,
+            fret: 4,
+            finger: 4,
+          },
+        ],
+
+        [
+          {
+            string: 4,
+            fret: 1,
+            finger: 1,
+          },
+          {
+            string: 1,
+            fret: 2,
+            finger: 2,
+          },
+          {
+            string: 4,
+            fret: 3,
+            finger: 3,
+          },
+        ],
+        [
+          {
+            string: 4,
+            fret: 1,
+            finger: 1,
+          },
+          {
+            string: 1,
+            fret: 2,
+            finger: 2,
+          },
+          {
+            string: 4,
+            fret: 4,
+            finger: 4,
+          },
+        ],
+
+      ],
+    },
+  ],
+}
+
+export default hazardPartA
