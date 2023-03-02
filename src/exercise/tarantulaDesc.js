473c71d6dd29a7ef61e7014b43ed33482a24afb0
Max Girkens <maxgirkens@gmail.com>
2023-03-02 07:09:29 +0100

    typo

--- src/exercise/tarantulaDesc.js
+++ src/exercise/tarantulaDesc.js
@@ -1,6 +1,6 @@
 const tarantulaDesc = {
   title: 'Tarantula Exercise in C-minor (Descending)',
-  description: 'Super useful and fun way to practice 9th arpeggios over the C-minor scale.\nDescenting version.\nTry plucking each arpeggio 1-1-1-2-1-2.',
+  description: 'Super useful and fun way to practice 9th arpeggios over the C-minor scale.\nDescending version.\nTry plucking each arpeggio 1-1-1-2-1-2.',
   bars: [
     {
       chord: [
