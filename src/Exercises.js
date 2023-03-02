6ed18623d10fbd32d291b6ad7c45d6ee00729817
Max Girkens <maxgirkens@gmail.com>
2023-03-02 09:05:39 +0100

    e melodic minor thirds

--- src/Exercises.js
+++ src/Exercises.js
@@ -1,9 +1,11 @@
 import tarantula from './exercise/tarantula'
 import tarantulaDesc from './exercise/tarantulaDesc'
+import eMelodicMinorThirds from './exercise/eMelodicMinorThirds'
 
 const exercises = {
   tarantula,
   tarantulaDesc,
+  eMelodicMinorThirds,
 }
 
 export default exercises
---
+++ src/exercise/eMelodicMinorThirds.js
@@ -0,0 +1,170 @@
+const eMelodicMinorThirds = {
+  title: 'E melodic minor scale in thirds',
+  description: 'Just a simple exercise to get used to the melodic minor scale in thirds',
+  bars: [
+    {
+      chord: [
+        'E3',
+        'G4',
+        'A4',
+        'F#5',
+      ],
+      ticks: [
+        [{
+          string: 2,
+          fret: 7,
+        }],
+        [{
+          string: 2,
+          fret: 10,
+        }],
+
+        [{
+          string: 2,
+          fret: 9,
+        }],
+        [{
+          string: 3,
+          fret: 7,
+        }],
+
+        [{
+          string: 2,
+          fret: 10,
+        }],
+        [{
+          string: 3,
+          fret: 9,
+        }],
+
+        [{
+          string: 3,
+          fret: 7,
+        }],
+        [{
+          string: 3,
+          fret: 10,
+        }],
+
+        [{
+          string: 3,
+          fret: 9,
+        }],
+        [{
+          string: 4,
+          fret: 8,
+        }],
+
+        [{
+          string: 3,
+          fret: 10,
+        }],
+        [{
+          string: 4,
+          fret: 9,
+        }],
+
+        [{
+          string: 4,
+          fret: 8,
+        }],
+        [{
+          string: 4,
+          fret: 11,
+        }],
+
+        [{
+          string: 4,
+          fret: 9,
+        }],
+        [{
+          string: 4,
+          fret: 12,
+        }],
+      ],
+    },
+    {
+      chord: [
+        'E3',
+        'G4',
+        'A4',
+        'F#5',
+      ],
+      ticks: [
+        [{
+          string: 4,
+          fret: 12,
+        }],
+        [{
+          string: 4,
+          fret: 9,
+        }],
+
+        [{
+          string: 4,
+          fret: 11,
+        }],
+        [{
+          string: 4,
+          fret: 8,
+        }],
+
+        [{
+          string: 4,
+          fret: 9,
+        }],
+        [{
+          string: 3,
+          fret: 10,
+        }],
+
+        [{
+          string: 4,
+          fret: 8,
+        }],
+        [{
+          string: 3,
+          fret: 9,
+        }],
+
+        [{
+          string: 3,
+          fret: 10,
+        }],
+        [{
+          string: 3,
+          fret: 7,
+        }],
+
+        [{
+          string: 3,
+          fret: 9,
+        }],
+        [{
+          string: 2,
+          fret: 10,
+        }],
+
+        [{
+          string: 3,
+          fret: 7,
+        }],
+        [{
+          string: 2,
+          fret: 9,
+        }],
+
+        [{
+          string: 2,
+          fret: 10,
+        }],
+        [{
+          string: 2,
+          fret: 7,
+        }],
+      ],
+    },
+  ],
+}
+
+export default eMelodicMinorThirds
