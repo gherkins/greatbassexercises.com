5f48cd6f27c7c1784943ad21933166c8c8e717a9
Max Girkens <maxgirkens@gmail.com>
2023-03-02 10:59:46 +0100

    hazard exercise

--- src/Exercises.js
+++ src/Exercises.js
@@ -1,11 +1,13 @@
 import tarantula from './exercise/tarantula'
 import tarantulaDesc from './exercise/tarantulaDesc'
 import eMelodicMinorThirds from './exercise/eMelodicMinorThirds'
+import hazardPartA from './exercise/hazardPartA'
 
 const exercises = {
   tarantula,
   tarantulaDesc,
   eMelodicMinorThirds,
+  hazardPartA,
 }
 
 export default exercises
--- src/exercise/hazardPartA.js
+++ src/exercise/hazardPartA.js
@@ -1,6 +1,6 @@
 const hazardPartA = {
   title: `Steve Bailey´s Hazard Exercise Part A`,
-  description: `"It's like yoga - for your fingers..."\nPractice at your own risk.`,
+  description: `"It's like yoga - for your fingers..."\nMention how fingers 1 & 2 stay in place.\nPractice at your own risk.`,
   bars: [
     {
       chord: [],
@@ -43,250 +43,550 @@ const hazardPartA = {
           fret: 1,
           finger: 1,
         }],
+        [
+          {
+            string: 4,
+            fret: 1,
+            finger: 1,
+          },
+          {
+            string: 4,
+            fret: 2,
+            finger: 2,
+          },
+        ],
+
+        [
+          {
+            string: 4,
+            fret: 1,
+            finger: 1,
+          },
+          {
+            string: 4,
+            fret: 3,
+            finger: 3,
+          },
+        ],
+        [
+          {
+            string: 4,
+            fret: 1,
+            finger: 1,
+          },
+          {
+            string: 4,
+            fret: 2,
+            finger: 2,
+          },
+        ],
+
+        [
+          {
+            string: 4,
+            fret: 1,
+            finger: 1,
+          },
+          {
+            string: 3,
+            fret: 3,
+            finger: 3,
+          },
+        ],
+        [
+          {
+            string: 4,
+            fret: 1,
+            finger: 1,
+          },
+          {
+            string: 3,
+            fret: 2,
+            finger: 2,
+          },
+        ],
+
+        [
+          {
+            string: 4,
+            fret: 1,
+            finger: 1,
+          },
+          {
+            string: 2,
+            fret: 3,
+            finger: 3,
+          },
+        ],
+        [
+          {
+            string: 4,
+            fret: 1,
+            finger: 1,
+          },
+          {
+            string: 2,
+            fret: 2,
+            finger: 2,
+          },
+        ],
+
+        [
+          {
+            string: 4,
+            fret: 1,
+            finger: 1,
+          },
+          {
+            string: 1,
+            fret: 3,
+            finger: 3,
+          },
+        ],
+        [
+          {
+            string: 4,
+            fret: 1,
+            finger: 1,
+          },
+          {
+            string: 1,
+            fret: 2,
+            finger: 2,
+          },
+        ],
+
+        [
+          {
+            string: 4,
+            fret: 1,
+            finger: 1,
+          },
+          {
+            string: 1,
+            fret: 2,
+            finger: 2,
+          },
+          {
+            string: 1,
+            fret: 3,
+            finger: 3,
+          },
+        ],
+        [
+          {
+            string: 4,
+            fret: 1,
+            finger: 1,
+          },
+          {
+            string: 1,
+            fret: 2,
+            finger: 2,
+          },
+          {
+            string: 1,
+            fret: 4,
+            finger: 4,
+          },
+        ],
+
+        [
+          {
+            string: 4,
+            fret: 1,
+            finger: 1,
+          },
+          {
+            string: 1,
+            fret: 2,
+            finger: 2,
+          },
+          {
+            string: 2,
+            fret: 3,
+            finger: 3,
+          },
+        ],
+        [
+          {
+            string: 4,
+            fret: 1,
+            finger: 1,
+          },
+          {
+            string: 1,
+            fret: 2,
+            finger: 2,
+          },
+          {
+            string: 2,
+            fret: 4,
+            finger: 4,
+          },
+        ],
+
+        [
+          {
+            string: 4,
+            fret: 1,
+            finger: 1,
+          },
+          {
+            string: 1,
+            fret: 2,
+            finger: 2,
+          },
+          {
+            string: 3,
+            fret: 3,
+            finger: 3,
+          },
+        ],
+        [
+          {
+            string: 4,
+            fret: 1,
+            finger: 1,
+          },
+          {
+            string: 1,
+            fret: 2,
+            finger: 2,
+          },
+          {
+            string: 3,
+            fret: 4,
+            finger: 4,
+          },
+        ],
+
+        [
+          {
+            string: 4,
+            fret: 1,
+            finger: 1,
+          },
+          {
+            string: 1,
+            fret: 2,
+            finger: 2,
+          },
+          {
+            string: 4,
+            fret: 3,
+            finger: 3,
+          },
+        ],
+        [
+          {
+            string: 4,
+            fret: 1,
+            finger: 1,
+          },
+          {
+            string: 1,
+            fret: 2,
+            finger: 2,
+          },
+          {
+            string: 4,
+            fret: 4,
+            finger: 4,
+          },
+        ],
+
+      ],
+    },
+    {
+      chord: [],
+      ticks: [
         [{
           string: 4,
-          fret: 2,
+          fret: 5,
           finger: 2,
+        }],
+        [{
+          string: 4,
+          fret: 4,
+          finger: 1,
+        }],
+
+        [{
+          string: 3,
+          fret: 5,
+          finger: 2,
+        }],
+        [{
+          string: 3,
+          fret: 4,
+          finger: 1,
+        }],
+
+        [{
+          string: 2,
+          fret: 5,
+          finger: 2,
+        }],
+        [{
+          string: 2,
+          fret: 4,
+          finger: 1,
+        }],
+
+        [{
+          string: 1,
+          fret: 5,
+          finger: 2,
+        }],
+        [{
+          string: 1,
+          fret: 4,
+          finger: 1,
         }],
 
         [
           {
-            string: 4,
-            fret: 1,
+            string: 1,
+            fret: 4,
             finger: 1,
           },
           {
-            string: 4,
-            fret: 3,
-            finger: 3,
+            string: 1,
+            fret: 5,
+            finger: 2,
           },
         ],
         [
           {
-            string: 4,
-            fret: 1,
+            string: 1,
+            fret: 4,
             finger: 1,
           },
           {
-            string: 4,
-            fret: 2,
-            finger: 2,
+            string: 1,
+            fret: 6,
+            finger: 3,
           },
         ],
 
         [
           {
-            string: 4,
-            fret: 1,
-            finger: 1,
-          },
-          {
-            string: 3,
-            fret: 3,
-            finger: 3,
-          },
-        ],
-        [
-          {
-            string: 4,
-            fret: 1,
-            finger: 1,
-          },
-          {
-            string: 3,
-            fret: 2,
-            finger: 2,
-          },
-        ],
-
-        [
-          {
-            string: 4,
-            fret: 1,
+            string: 1,
+            fret: 4,
             finger: 1,
           },
           {
             string: 2,
-            fret: 3,
-            finger: 3,
-          },
-        ],
-        [
-          {
-            string: 4,
-            fret: 1,
-            finger: 1,
-          },
-          {
-            string: 2,
-            fret: 2,
+            fret: 5,
             finger: 2,
           },
         ],
-
         [
-          {
-            string: 4,
-            fret: 1,
-            finger: 1,
-          },
-          {
-            string: 1,
-            fret: 3,
-            finger: 3,
-          },
-        ],
-        [
-          {
-            string: 4,
-            fret: 1,
-            finger: 1,
-          },
-          {
-            string: 1,
-            fret: 2,
-            finger: 2,
-          },
-        ],
-
-
-        [
-          {
-            string: 4,
-            fret: 1,
-            finger: 1,
-          },
-          {
-            string: 1,
-            fret: 2,
-            finger: 2,
-          },
-          {
-            string: 1,
-            fret: 3,
-            finger: 3,
-          },
-        ],
-        [
-          {
-            string: 4,
-            fret: 1,
-            finger: 1,
-          },
-          {
-            string: 1,
-            fret: 2,
-            finger: 2,
-          },
           {
             string: 1,
             fret: 4,
-            finger: 4,
-          },
-        ],
-
-        [
-          {
-            string: 4,
-            fret: 1,
             finger: 1,
           },
           {
-            string: 1,
-            fret: 2,
-            finger: 2,
-          },
-          {
             string: 2,
-            fret: 3,
+            fret: 6,
             finger: 3,
           },
         ],
+
         [
           {
-            string: 4,
-            fret: 1,
+            string: 1,
+            fret: 4,
             finger: 1,
           },
           {
-            string: 1,
-            fret: 2,
+            string: 3,
+            fret: 5,
             finger: 2,
           },
+        ],
+        [
           {
-            string: 2,
+            string: 1,
             fret: 4,
-            finger: 4,
+            finger: 1,
+          },
+          {
+            string: 3,
+            fret: 6,
+            finger: 3,
           },
         ],
 
         [
           {
-            string: 4,
-            fret: 1,
+            string: 1,
+            fret: 4,
             finger: 1,
           },
           {
+            string: 4,
+            fret: 5,
+            finger: 2,
+          },
+        ],
+        [
+          {
             string: 1,
-            fret: 2,
+            fret: 4,
+            finger: 1,
+          },
+          {
+            string: 4,
+            fret: 5,
+            finger: 2,
+          },
+          {
+            string: 4,
+            fret: 6,
+            finger: 3,
+          },
+        ],
+
+        [
+          {
+            string: 1,
+            fret: 4,
+            finger: 1,
+          },
+          {
+            string: 4,
+            fret: 5,
+            finger: 2,
+          },
+          {
+            string: 4,
+            fret: 7,
+            finger: 4,
+          },
+        ],
+        [
+          {
+            string: 1,
+            fret: 4,
+            finger: 1,
+          },
+          {
+            string: 4,
+            fret: 5,
+            finger: 2,
+          },
+          {
+            string: 4,
+            fret: 6,
+            finger: 3,
+          },
+        ],
+
+        [
+          {
+            string: 1,
+            fret: 4,
+            finger: 1,
+          },
+          {
+            string: 4,
+            fret: 5,
             finger: 2,
           },
           {
             string: 3,
-            fret: 3,
-            finger: 3,
+            fret: 7,
+            finger: 4,
           },
         ],
         [
           {
-            string: 4,
-            fret: 1,
+            string: 1,
+            fret: 4,
             finger: 1,
           },
           {
-            string: 1,
-            fret: 2,
+            string: 4,
+            fret: 5,
             finger: 2,
           },
           {
             string: 3,
-            fret: 4,
-            finger: 4,
+            fret: 6,
+            finger: 3,
           },
         ],
 
         [
           {
-            string: 4,
-            fret: 1,
+            string: 1,
+            fret: 4,
             finger: 1,
           },
           {
-            string: 1,
-            fret: 2,
+            string: 4,
+            fret: 5,
             finger: 2,
           },
           {
-            string: 4,
-            fret: 3,
-            finger: 3,
+            string: 2,
+            fret: 7,
+            finger: 4,
           },
         ],
         [
           {
-            string: 4,
-            fret: 1,
+            string: 1,
+            fret: 4,
             finger: 1,
           },
           {
-            string: 1,
-            fret: 2,
+            string: 4,
+            fret: 5,
             finger: 2,
           },
           {
-            string: 4,
+            string: 2,
+            fret: 6,
+            finger: 3,
+          },
+        ],
+
+        [
+          {
+            string: 1,
             fret: 4,
+            finger: 1,
+          },
+          {
+            string: 4,
+            fret: 5,
+            finger: 2,
+          },
+          {
+            string: 1,
+            fret: 7,
             finger: 4,
+          },
+        ],
+        [
+          {
+            string: 1,
+            fret: 4,
+            finger: 1,
+          },
+          {
+            string: 4,
+            fret: 5,
+            finger: 2,
+          },
+          {
+            string: 1,
+            fret: 6,
+            finger: 3,
           },
         ],
 
